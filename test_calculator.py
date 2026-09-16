35f5827d04acff96750691749dfdc0eff268d7ca
serotonin <samilnafisa.email@example.com>
2026-09-16 10:03:38 +0500

    fix_format

--- test_calculator.py
+++ test_calculator.py
@@ -4,11 +4,14 @@ from calculator import add, divide, multiply, subtract
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_subtract():
     assert subtract(5, 3) == 2
 
+
 def test_multiply():
     assert multiply(2, 3) == 6
+
 
 def test_divide():
     assert divide(6, 2) == 3
